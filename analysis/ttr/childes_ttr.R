8994f9a0d1d9c497bb0d814fdbd0591c77807847
Kennedy Casey <kennedybcasey@gmail.com>
2021-08-16 17:23:44 -0500

    add initial type:token ratio analyses

---
+++ analysis/ttr/childes_ttr.R
@@ -0,0 +1,206 @@
+library(childesr)
+library(tidyverse)
+library(data.table)
+library(stringr)
+library(readr)
+library(ggpubr)
+library(lme4)
+library(lmerTest)
+
+# get all utterances from providence corpus
+childes_utterances = data.table(get_utterances(collection = "Eng-NA"))
+
+utterances <- childes_utterances %>%
+  filter(target_child_age < 60) %>%
+  mutate(gloss = paste0(' ', tolower(gloss), ' '), 
+         age = round(target_child_age, digits = 0))
+
+items <- read_csv("data_prep/item_info/candidate_items_new.csv") %>%
+  pull(word)
+
+ids_forms <- read_csv("data_prep/item_info/candidate_items_new.csv") %>%
+  filter(form == "ids") %>%
+  pull(word)
+
+ads_forms <- read_csv("data_prep/item_info/candidate_items_new.csv") %>%
+  filter(form == "ads") %>%
+  pull(word)
+
+
+pairs <- read_csv("data_prep/item_info/candidate_items_new.csv") %>%
+  select(word, pair)
+
+colors <- c("ids" = "#C1292E", "ads" = "#235789")
+
+# create empty list to be populated
+get_ttr <- list() 
+
+# loop over all items to get num_tokens for all utterances containing a target word
+for(i in items){
+  if (str_detect(i, "ey")) {
+    root <- paste(gsub("ey", "", i))
+    
+    utts_w_target <- utterances %>%
+      filter(str_detect(gloss, regex(paste0(" ", root, "ey | ", root, "ie | ",
+                                                         root, "eys | ", root, "ies | ",
+                                                         root, "ey's | ", root, "ie's ")))) %>%
+      select(target_child_id, transcript_id, id, age, speaker_role, num_tokens, gloss) %>%
+      mutate(item = paste0(i), 
+             form = case_when(
+               i %in% ids_forms ~ "ids", 
+               i %in% ads_forms ~ "ads"), 
+             pair = paste0((filter(pairs, word == i))$pair))
+  }
+  
+  else if (str_detect(i, "y") & !str_detect(i, "ey")) {
+    root <- paste(gsub("y", "", i))
+    utts_w_target <- utterances %>%
+      filter(str_detect(gloss, regex(paste0(" ", root, "y | ", root, "ie | ",
+                                                         root, "ys | ", root, "ies | ",
+                                                         root, "y's | ", root, "ie's ")))) %>%
+      select(target_child_id, transcript_id, id, age, speaker_role, num_tokens, gloss) %>%
+      mutate(item = paste0(i), 
+             form = case_when(
+               i %in% ids_forms ~ "ids", 
+               i %in% ads_forms ~ "ads"),
+             pair = paste0((filter(pairs, word == i))$pair))
+  }
+  
+  else if (str_detect(i, "ie")) {
+    root <- paste(gsub("ie", "", i))
+    utts_w_target <- utterances %>%
+      filter(str_detect(gloss, regex(paste0(" ", root, "y | ", root, "ie | ",
+                                                         root, "ys | ", root, "ies | ",
+                                                         root, "y's | ", root, "ie's ")))) %>%
+      select(target_child_id, transcript_id, id, age, speaker_role, num_tokens, gloss) %>%
+      mutate(item = paste0(i), 
+             form = case_when(
+               i %in% ids_forms ~ "ids", 
+               i %in% ads_forms ~ "ads"), 
+             pair = paste0((filter(pairs, word == i))$pair))
+  }
+  
+  else if (i == "night night"){
+    utts_w_target <- utterances %>%
+      filter(str_detect(gloss, regex(paste0(" night night | night-night | night nights | night-nights ")))) %>%
+      select(target_child_id, transcript_id, id, age, speaker_role, num_tokens, gloss) %>%
+      mutate(item = paste0(i), 
+             form = case_when(
+               i %in% ids_forms ~ "ids", 
+               i %in% ads_forms ~ "ads"), 
+             pair = paste0((filter(pairs, word == i))$pair))
+  }
+  
+  else if (i == "goodnight"){
+    utts_w_target <- utterances %>%
+      filter(str_detect(gloss, regex(paste0(" goodnight | good night | good-night ")))) %>%
+      select(target_child_id, transcript_id, id, age, speaker_role, num_tokens, gloss) %>%
+      mutate(item = paste0(i), 
+             form = case_when(
+               i %in% ids_forms ~ "ids", 
+               i %in% ads_forms ~ "ads"), 
+             pair = paste0((filter(pairs, word == i))$pair))
+  }
+  
+  else utts_w_target <- utterances %>%
+      filter(str_detect(gloss, regex(paste0(" ", i, " | ", i, "s | ", i, "'s ")))) %>%
+      select(target_child_id, transcript_id, id, age, speaker_role, num_tokens, gloss) %>%
+      mutate(item = paste0(i), 
+             form = case_when(
+               i %in% ids_forms ~ "ids", 
+               i %in% ads_forms ~ "ads"), 
+             pair = paste0((filter(pairs, word == i))$pair))
+  
+  get_ttr[[i]] <- utts_w_target
+  
+}
+
+ttr <- do.call(rbind, get_ttr)
+ttr$form <- factor(mlu$form, levels = c("ids", "ads"))
+
+ttr %>%
+  mutate(num_types = length(unique(strsplit(gloss, " ")))) %>%
+  group_by(item, age) %>%
+  summarize(mlu = mean(num_tokens), 
+            pair = pair, 
+            form = form) %>%
+  distinct() %>%
+  ggplot(aes(x = age, y = mlu, color = form, fill = form)) +
+  facet_wrap(.~pair, nrow = 5) +
+  geom_point() +
+  geom_smooth() +
+  scale_color_manual(values = colors) +
+  scale_fill_manual(values = colors) +
+  labs(x = "age (months)", y = "MLUw") +
+  theme_test(base_size = 15)
+ggsave("plots/mlu/mlu_over_time.jpg", height = 15, width = 12, dpi = 300)
+
+
+m <- lmer(num_tokens ~ form*age + (1|item) + (1|target_child_id), data = mlu)
+summary(m)
+
+mlu_byword <- mlu %>%
+  group_by(item) %>%
+  summarize(mlu = mean(num_tokens), 
+            pair = pair, 
+            form = form) %>%
+  distinct()
+
+mlu_byword_summary <- mlu_byword %>%
+  group_by(form) %>%
+  summarize(mean = mean(mlu), 
+            se = sd(mlu)/sqrt(length(mlu)), 
+            ymin = mean - se, 
+            ymax = mean + se)
+
+ggplot() +
+  geom_bar(data = mlu_byword_summary, aes(x = form, y = mean, color = form, fill = form), 
+           stat = "identity", width = 0.7, alpha = 0.5, size = 1.5) +
+  geom_line(data = mlu_byword, aes(x = form, y = mlu, group = pair)) +
+  geom_point(data = mlu_byword, aes(x = form, y = mlu, color = form, fill = form)) +
+  geom_errorbar(data = mlu_byword_summary, aes(x = form, ymin = mean-se, ymax = mean+se), 
+                stat = "identity", width = 0.15, size = 0.75, position = position_dodge(0.9)) +
+  scale_fill_manual(values = colors) +
+  scale_color_manual(values = colors) +
+  labs(x = "form", y = "MLUw") +
+  theme_test(base_size = 15) +
+  theme(legend.position = "none")
+
+ggplot() +
+  geom_line(data = mlu_byword, aes(x = form, y = mlu, group = pair), 
+            color = "#D3D3D3", size = 1) +
+  geom_point(data = mlu_byword, aes(x = form, y = mlu), 
+             color = "#D3D3D3", size = 2) +
+  geom_pointrange(data = mlu_byword_summary, aes(x = form, y = mean, ymin = mean-se, ymax = mean+se, color = form, fill = form), 
+                  stat = "identity", size = 1.5) +
+  scale_fill_manual(values = colors) +
+  scale_color_manual(values = colors) +
+  labs(x = "form", y = "MLUw", title = "CHILDES (<60m)") +
+  ylim(3, 9) +
+  theme_test(base_size = 15) +
+  theme(legend.position = "none", plot.title = element_text(hjust = 0.5))
+ggsave("plots/mlu/mlu_overall.jpg", height = 5, width = 4, dpi = 300)
+
+# higher mlu for ads
+t.test(mlu ~ form, data = mlu_byword, paired = TRUE)
+shapiro.test(filter(mlu_byword, form == "ids")$mlu) #check for normality
+shapiro.test(filter(mlu_byword, form == "ads")$mlu) 
+
+
+mlu %>%
+  group_by(item, age) %>%
+  summarize(mlu = mean(num_tokens), 
+            pair = pair, 
+            form = form) %>%
+  distinct() %>%
+  ungroup() %>%
+  group_by(age, form) %>%
+  summarize(mlu = mean(mlu)) %>%
+  ggplot(aes(x = age, y = mlu, color = form, fill = form)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  scale_color_manual(values = colors) +
+  scale_fill_manual(values = colors) +
+  labs(x = "age (months)", y = "MLUw") +
+  theme_test(base_size = 15) +
+  theme(legend.position = "none")
