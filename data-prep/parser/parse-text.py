431d5ef77e61b77b09e71dc9870520623c443725
Kennedy Casey <kennedybcasey@gmail.com>
2021-12-22 13:19:13 -0600

    fix codec issue + add loop

--- data-prep/parser/parse-text.py
+++ data-prep/parser/parse-text.py
@@ -3,6 +3,22 @@
 import spacy, benepar
 import pandas as pd
 
+target_words = ["doggy", "dog", 
+                "kitty", "cat", 
+                "tummy", "stomach", 
+                "daddy", "dad", 
+                "mommy", "mom", 
+                "bunny", "rabbit",
+                "duckie", "duck", 
+                "blankie", "blanket", 
+                "froggy", "frog", 
+                "potty", "bathroom", 
+                "night-night", "goodnight", 
+                "dolly", "doll", 
+                "horsey", "horse", 
+                "piggy", "pig", 
+                "birdie", "bird"]
+
 nlp = spacy.load("en_core_web_md")
 
 if spacy.__version__.startswith('2'):
@@ -10,16 +26,17 @@ if spacy.__version__.startswith('2'):
 else:
     nlp.add_pipe("benepar", config={"model": "benepar_en3"})
 
-utts_df = pd.read_csv("other-utterances.csv", low_memory = False)
+for t in target_words:
+    utts_df = pd.read_csv("../../data/" + t + ".csv", 
+    low_memory = False, encoding='latin-1')
+    pos_tags = []
 
-pos_tags = []
-
-for i in range(len(utts_df)):
-    gloss = str(utts_df.values[i][2])
-    doc = nlp(gloss)
-    sent = list(doc.sents)[0]
-    pos = sent._.parse_string
-    pos_tags.append(pos)
+    for i in range(len(utts_df)):
+        gloss = str(utts_df.values[i][2])
+        doc = nlp(gloss)
+        sent = list(doc.sents)[0]
+        pos = sent._.parse_string
+        pos_tags.append(pos)
     
-utts_df["parsed_gloss"] = pos_tags
-utts_df.to_csv("other-utterances-parsed.csv")
+    utts_df["parsed_gloss"] = pos_tags
+    utts_df.to_csv(t + "-parsed.csv")
