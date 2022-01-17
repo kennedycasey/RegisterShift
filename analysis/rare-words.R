5ca3648332d40f5fbe2b42d7f765fef09a182752
Kennedy Casey <kennedybcasey@gmail.com>
2022-01-16 18:41:36 -0600

    update rare words analysis

--- analysis/rare-words.R
+++ analysis/rare-words.R
@@ -4,10 +4,14 @@ library(childesr)
 library(wordbankr)
 library(lme4)
 library(lmerTest)
-source("data-prep/overall/functions.R")
+
+path = "data/childes-byword/"
+filenames <- list.files(path, "*.csv")
+files <- lapply(paste0(path, filenames), read_csv)
+utterances <- do.call(rbind, files)
 
 childes_utterances = data.table(get_utterances(collection = "Eng-NA"))
-get_utts_w_target(childes_utterances, items)
+#get_utts_w_target(childes_utterances, items)
 
 function_words <- get_item_data(language = "English (American)", form = "WS") %>%
   filter(lexical_category == "function_words") %>%
@@ -20,27 +24,26 @@ words <- childes_utterances %>%
   filter(target_child_age < 84 & speaker_role != "Target_Child") %>%
   mutate(word = tolower(stem)) %>%
   separate_rows(word, sep = " ") %>%
-  filter(word %notin% function_words) %>%
+  #filter(word %notin% function_words) %>%
   group_by(word) %>%
   summarize(raw_freq = n()) %>%
   mutate(row_number = row_number()) %>%
-  filter(row_number != 1 & raw_freq > 1) %>%
-  mutate(freq = scale(-raw_freq))
+  filter(row_number != 1 & raw_freq > 1)
 
 total <- sum(words$raw_freq)
 
 relative_freq <- words %>%
   select(word, raw_freq) %>%
   mutate(freq = raw_freq/total, 
-         freq = scale(-freq))
+         freq = -log(freq))
 
 mean_freq <- utterances %>%
   filter(speaker_type == "other") %>%
   mutate(word = tolower(stem)) %>%
   separate_rows(word, sep = " ") %>%
-  filter(word %notin% function_words) %>%
-  filter(!str_detect(word, " |dog|cat|pig|stomach|mommy|daddy|mom|dad|frog|blanket|duck|rabbit|bunny|potty|bathroom|doll|horse|bird")) %>%
-  left_join(words, by = "word") %>%
+  #filter(word %notin% function_words) %>%
+  #filter(!str_detect(word, " |dog|cat|pig|stomach|mommy|daddy|mom|dad|frog|blanket|duck|rabbit|bunny|potty|bathroom|doll|horse|bird")) %>%
+  left_join(relative_freq, by = "word") %>%
   group_by(id) %>%
   summarize(freq = mean(freq, na.rm = TRUE))
 
@@ -52,8 +55,7 @@ freq_other <- utterances %>%
   filter(!is.na(freq_scaled))
 
 m <- glmer(form_numeric ~ freq_scaled * age_scaled + 
-             (1|pair) + 
-             (1|speaker_id), 
+              (freq_scaled * age_scaled|pair) + (freq_scaled * age_scaled|speaker_id),
            data = freq_other,
            family = binomial, 
            control = glmerControl(optimizer = "bobyqa"))
