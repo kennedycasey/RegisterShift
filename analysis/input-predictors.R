4ad808571465be75895a74192dadfbf939a167af
Kennedy Casey <kennedybcasey@gmail.com>
2022-01-20 15:41:38 -0600

    update random effects structure

--- analysis/input-predictors.R
+++ analysis/input-predictors.R
@@ -11,12 +11,14 @@ input <- read_csv("data/full-input.csv") %>%
 
 # no effect of mean pitch on form
 m.pitch.mean <- glmer(form_numeric ~ pitch_mean_scaled * age_scaled + 
-             (pitch_mean_scaled * age_scaled|pair) + 
-             (pitch_mean_scaled * age_scaled|speaker_id), 
-           data = pitch, 
-           family = binomial, 
-           control = glmerControl(optimizer = "bobyqa"))
-summary(m)
+                        (pitch_mean_scaled|pair) + 
+                        (age_scaled|pair) +
+                        (pitch_mean_scaled|speaker_id) + 
+                        (age_scaled|speaker_id),
+                      data = pitch, 
+                      family = binomial, 
+                      control = glmerControl(optimizer = "bobyqa"))
+summary(m.pitch.mean)
 
 tidy(m.pitch.mean) %>%
   filter(effect == "fixed") %>%
@@ -24,24 +26,28 @@ tidy(m.pitch.mean) %>%
 
 # marginal effect of pitch range on form
 m.pitch.range <- glmer(form_numeric ~ pitch_range_scaled * age_scaled + 
-             (pitch_range_scaled * age_scaled|pair) + 
-             (pitch_range_scaled * age_scaled|speaker_id), 
-           data = pitch_info, 
-           family = binomial, 
-           control = glmerControl(optimizer = "bobyqa"))
+                         (pitch_range_scaled|pair) + 
+                         (age_scaled|pair) +
+                         (pitch_range_scaled|speaker_id) + 
+                         (age_scaled|speaker_id),
+                       data = pitch_info, 
+                       family = binomial, 
+                       control = glmerControl(optimizer = "bobyqa"))
 summary(m.pitch.range)
 
 tidy(m.pitch.range) %>%
   filter(effect == "fixed") %>%
   write_csv("analysis/model-outputs/input/pitch-range.csv")
 
-# sig effect of utterance length on form
+# sig effect of lexical complexity on form
 m.complexity <- glmer(form_numeric ~ complexity_scaled * age_scaled + 
-                    (complexity_scaled * age_scaled|pair) + 
-                    (complexity_scaled * age_scaled|speaker_id), 
-                  data = input, 
-                  family = binomial, 
-                  control = glmerControl(optimizer = "bobyqa"))
+                        (complexity_scaled|pair) + 
+                        (age_scaled|pair) +
+                        (complexity_scaled|speaker_id) + 
+                        (age_scaled|speaker_id),
+                      data = input, 
+                      family = binomial, 
+                      control = glmerControl(optimizer = "bobyqa"))
 summary(m.complexity)
 
 tidy(m.complexity) %>%
@@ -50,8 +56,10 @@ tidy(m.complexity) %>%
 
 # sig effect of utterance length on form
 m.length <- glmer(form_numeric ~ length_scaled * age_scaled + 
-                    (length_scaled * age_scaled|pair) + 
-                    (length_scaled * age_scaled|speaker_id), 
+                    (length_scaled|pair) + 
+                    (age_scaled|pair) +
+                    (length_scaled|speaker_id) + 
+                    (age_scaled|speaker_id),
                   data = input, 
                   family = binomial, 
                   control = glmerControl(optimizer = "bobyqa"))
