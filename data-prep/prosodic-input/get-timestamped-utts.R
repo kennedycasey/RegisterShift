904a3fd82f5c21f5b91196326efce6f5ff32103e
Kennedy Casey <kennedybcasey@gmail.com>
2022-03-31 10:56:26 -0500

    add timestamped child utts

--- data-prep/prosodic-input/get-timestamped-utts.R
+++ data-prep/prosodic-input/get-timestamped-utts.R
@@ -56,88 +56,14 @@ transcripts <- raw_transcripts %>%
   bind_rows(brent_transcripts, gleason_transcripts, newmanratner_transcripts, rollins_transcripts)
 
 # get timestamped utterances containing target word
-childes_utterances = data.table(get_utterances(collection = "Eng-NA"))
-
-utterances <- childes_utterances %>%
-  filter(target_child_age < 84) %>%
-  mutate(gloss = paste0(' ', tolower(gloss), ' '), 
-         age = round(target_child_age, digits = 0))
+timestamped <- read_csv("data/childes-input.csv") %>%
+  filter(!is.na(media_start) & !is.na(media_end))
 
 items <- read_csv("data-prep/overall/item-info.csv") %>%
   pull(word)
 
 for (i in items) {
-  if (str_detect(i, "ey")) {
-    root <- paste(gsub("ey", "", i))
-    utterances[str_detect(gloss, regex(paste0(" ", root, "ey | ", root, "ie | ",
-                                              root, "eys | ", root, "ies | ",
-                                              root, "ey's | ", root, "ie's "))), 
-               paste0(i) := str_count(gloss, regex(paste0(" ", root, "ey | ", root, "ie | ",
-                                                          root, "eys | ", root, "ies | ",
-                                                          root, "ey's | ", root, "ie's ")))]
-  }
-  
-  else if (str_detect(i, "y") & !str_detect(i, "ey")) {
-    root <- paste(gsub("y", "", i))
-    utterances[str_detect(gloss, regex(paste0(" ", root, "y | ", root, "ie | ",
-                                              root, "ys | ", root, "ies | ",
-                                              root, "y's | ", root, "ie's "))), 
-               paste0(i) := str_count(gloss, regex(paste0(" ", root, "y | ", root, "ie | ",
-                                                          root, "ys | ", root, "ies | ",
-                                                          root, "y's | ", root, "ie's ")))]
-  }
-  
-  else if (str_detect(i, "ie")) {
-    root <- paste(gsub("ie", "", i))
-    utterances[str_detect(gloss, regex(paste0(" ", root, "y | ", root, "ie | ",
-                                              root, "ys | ", root, "ies | ",
-                                              root, "y's | ", root, "ie's "))), 
-               paste0(i) := str_count(gloss, regex(paste0(" ", root, "y | ", root, "ie | ",
-                                                          root, "ys | ", root, "ies | ",
-                                                          root, "y's | ", root, "ie's ")))]
-  }
-  
-  else if (i == "night night") {
-    utterances[str_detect(gloss, regex(paste0(" night night | night-night | night nights | night-nights "))), 
-               paste0(i) := str_count(gloss, regex(paste0(" night night | night-night | night nights | night-nights ")))]
-  }
-  
-  else if (i == "goodnight") {
-    utterances[str_detect(gloss, regex(paste0(" goodnight | good night | good-night "))), 
-               paste0(i) := str_count(gloss, regex(paste0(" goodnight | good night | good-night ")))]
-  }
-  
-  else utterances[str_detect(gloss, regex(paste0(" ", i, " | ", i, "s | ", i, "'s "))), 
-                  paste0(i) := str_count(gloss, regex(paste0(" ", i, " | ", i, "s | ", i, "'s ")))]
-}
-
-timestamped <- data.frame(utterances) %>%
-  filter(!is.na(media_start) & !is.na(media_end) & speaker_role != "Target_Child")
-
-get_timestamped_utts <- list()
-
-for (i in items) {
-  if (i == "night night") {
-    subset <- timestamped %>%
-      filter(!is.na(night.night)) %>%
-      mutate(item = paste0(i)) %>%
-      select(item, transcript_id, corpus_name, speaker_id,
-             age, target_child_name, target_child_id, 
-             media_start, media_end)
-  }
-  
-  else subset <- timestamped %>%
-    filter(!is.na(eval(as.symbol(i)))) %>%
-    mutate(item = paste0(i)) %>%
-      select(item, transcript_id, corpus_name, speaker_id,
-             age, target_child_name, target_child_id, 
-             media_start, media_end)
-  
-  get_timestamped_utts[[i]] <- subset
-}
-
-for (i in items) {
-  timestamped <- do.call(rbind, get_timestamped_utts) %>%
+  utterances <- timestamped %>%
     filter(item == i) %>%
     select(-target_child_name) %>%
     left_join(transcripts, by = c("transcript_id", "corpus_name")) %>%
@@ -147,5 +73,5 @@ for (i in items) {
            pitch_max = "", 
            pitch_range = "")
   
-  write_csv(timestamped, paste0("data-prep/prosodic-input/timestamped/", i, ".csv"))
+  write_csv(utterances, paste0("data-prep/prosodic-input/timestamped/", i, ".csv"))
 }
