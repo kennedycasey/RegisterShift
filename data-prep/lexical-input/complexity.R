69a6c165181453f297774f42787b8542a13e63b5
Kennedy Casey <kennedybcasey@gmail.com>
2022-04-01 09:28:56 -0500

    prep aoa ratings

--- data-prep/lexical-input/complexity.R
+++ data-prep/lexical-input/complexity.R
@@ -9,56 +9,54 @@ library(lmerTest)
 library(performance)
 source("data-prep/overall/functions.R")
 
-aoa <- read_csv("data-prep/lexical-input/aoa.csv")
-
-for (i in c("child", "other")) {
-  utterances <- read_csv("data/childes-input.csv") %>%
-    filter(speaker_type == i) %>%
-    select(corpus_name, id, speaker_id, target_child_id, age, 
-           pair, item, form, form_numeric, stem)
+for (type in c("wordbank", "ratings")) {
+  aoa <- read_csv(paste0("data-prep/lexical-input/aoa-", type, ".csv"))
   
-  known_words <- utterances %>%
-    mutate(word = tolower(stem)) %>%
-    separate_rows(word, sep = " ") %>%
-    filter(!str_detect(word, " |dog|cat|pig|stomach|mommy|daddy|mom|dad|frog|blanket|duck|rabbit|bunny|potty|bathroom|doll|horse|bird")) %>%
-    left_join(aoa, by = "word") %>%
-    mutate(known = case_when(
-      !is.na(aoa) & aoa <= age ~ "known",
-      !is.na(aoa) & aoa > age ~ "not_known")) %>%
-    filter(!is.na(known)) %>%
-    group_by(id, known) %>%
-    summarize(n = n()) %>%
-    ungroup() %>%
-    pivot_wider(names_from = "known", values_from = "n") %>%
-    group_by(id) %>%
-    summarize(known_prop = known/(known + not_known))
+  for (i in c("child", "other")) {
+    utterances <- read_csv("data/childes-input.csv") %>%
+      filter(speaker_type == i)
+    
+    known_words <- utterances %>%
+      mutate(word = tolower(stem)) %>%
+      separate_rows(word, sep = " ") %>%
+      filter(!str_detect(word, " |dog|cat|pig|stomach|mommy|daddy|mom|dad|frog|blanket|duck|rabbit|bunny|potty|bathroom|doll|horse|bird")) %>%
+      left_join(aoa, by = "word") %>%
+      mutate(known = case_when(
+        !is.na(aoa) & aoa <= age ~ "known",
+        !is.na(aoa) & aoa > age ~ "not_known")) %>%
+      filter(!is.na(known)) %>%
+      group_by(id, known) %>%
+      summarize(n = n()) %>%
+      ungroup() %>%
+      pivot_wider(names_from = "known", values_from = "n") %>%
+      group_by(id) %>%
+      summarize(known_prop = known/(known + not_known))
+    
+    known_props <- utterances %>%
+      left_join(known_words, by = c("id")) %>%
+      mutate(complexity = -log(known_prop))
   
-  known_props <- utterances %>%
-    left_join(known_words, by = c("id")) %>%
-    mutate(complexity = -log(known_prop)) %>%
-    select(corpus_name, id, speaker_id, target_child_id, age, 
-           pair, item, form, form_numeric, complexity)
+    filename <- ifelse(i == "child", paste0("data/input/complexity-", type, "-child.csv"), 
+                       paste0("data/input/complexity-", type, ".csv"))
+    
+    write_csv(known_props, filename)
+    
+    # check NAs
+    complexity_nas <- utterances %>%
+      mutate(word = tolower(stem)) %>%
+      separate_rows(word, sep = " ") %>%
+      filter(!str_detect(word, " |dog|cat|pig|stomach|mommy|daddy|mom|dad|frog|blanket|duck|rabbit|bunny|potty|bathroom|doll|horse|bird")) %>%
+      left_join(aoa, by = "word") %>%
+      mutate(na = ifelse(is.na(aoa), "na", "not_na")) %>%
+      group_by(id, na) %>%
+      summarize(n = n()) %>%
+      ungroup() %>%
+      pivot_wider(names_from = "na", values_from = "n") %>%
+      mutate(na = ifelse(is.na(na), 0, na), 
+             not_na = ifelse(is.na(not_na), 0, not_na)) %>%
+      group_by(id) %>%
+      summarize(na_prop = na/(na + not_na))
   
-  filename <- ifelse(i == "child", "data/input/complexity-child.csv", 
-                     "data/input/complexity.csv")
-  
-  write_csv(known_props, filename)
-  
-  # check NAs
-  complexity_nas <- utterances %>%
-    mutate(word = tolower(stem)) %>%
-    separate_rows(word, sep = " ") %>%
-    filter(!str_detect(word, " |dog|cat|pig|stomach|mommy|daddy|mom|dad|frog|blanket|duck|rabbit|bunny|potty|bathroom|doll|horse|bird")) %>%
-    left_join(aoa, by = "word") %>%
-    mutate(na = ifelse(is.na(aoa), "na", "not_na")) %>%
-    group_by(id, na) %>%
-    summarize(n = n()) %>%
-    ungroup() %>%
-    pivot_wider(names_from = "na", values_from = "n") %>%
-    mutate(na = ifelse(is.na(na), 0, na), 
-           not_na = ifelse(is.na(not_na), 0, not_na)) %>%
-    group_by(id) %>%
-    summarize(na_prop = na/(na + not_na))
-
-  mean(complexity_nas$na_prop)
+    mean(complexity_nas$na_prop)
+  }
 }
--- data-prep/lexical-input/get-aoa.R
+++ data-prep/lexical-input/get-aoa.R
@@ -1,8 +1,10 @@
 library(wordbankr)
 library(tidyverse)
 library(ggrepel)
+library(readxl)
 
-# get all unique words in cdi
+# wordbank ----------------------------------------------------------------
+# get all words on the CDI
 get_items <- get_item_data(language = "English (American)", form = "WS") %>%
   filter(type == "word")
 
@@ -12,12 +14,12 @@ item_ids <- get_items %>%
   mutate(word = str_remove_all(uni_lemma, "(\\s*\\(\\w+\\))")) %>%
   select(num_item_id, word)
 
-# get all administrations of eng ws cdi
+# get all administrations CDI-W&S (Eng-NA)
 eng_data <- get_instrument_data(language = "English (American)", form = "WS", 
                                 items = items, administrations = TRUE, iteminfo = TRUE) %>%
   filter(!is.na(age))
 
-# get aoa data
+# get AoA data
 aoa_data <- fit_aoa(eng_data, measure = "understands", 
                     method = "glm", proportion = 0.5) 
 
@@ -29,7 +31,7 @@ aoa <- aoa_data %>%
   group_by(word) %>%
   summarize(aoa = mean(aoa))
 
-write_csv(aoa, "data-prep/lexical-input/aoa.csv")
+write_csv(aoa, "data-prep/lexical-input/aoa-wordbank.csv")
 
 # get production percentiles
 production_data <- get_instrument_data(language = "English (American)", 
@@ -50,4 +52,11 @@ production_percentile <- production_data %>%
          not_known = ifelse(is.na(not_known), 0, not_known),
          prod_prop = known/(known + not_known))
 
-write_csv(production_percentile, "data-prep/lexical-input/prod-norms.csv")
+write_csv(production_percentile, "data-prep/lexical-input/production-norms-wordbank.csv")
+
+
+# adult ratings -----------------------------------------------------------
+aoa2 <- read_xlsx("data-prep/lexical-input/13428_2013_348_MOESM1_ESM.xlsx") %>%
+  rename(word = Word) %>%
+  mutate(aoa = as.numeric(as.character(Rating.Mean))) %>%
+  filter(!is.na(aoa))
