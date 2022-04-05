9bcd76081201bdf8726d327b309a8b2fa2f1df84
Kennedy Casey <kennedybcasey@gmail.com>
2022-04-04 23:45:03 -0500

    store child and adult utts separately for verb phrase counts

--- data-prep/syntactic-input/verbs.R
+++ data-prep/syntactic-input/verbs.R
@@ -7,10 +7,16 @@ library(lmerTest)
 path_to_verb_info = "data-prep/syntactic-input/parsed/"
 parsed_filenames <- list.files(path = path_to_verb_info, pattern = "*.csv")
 verb_files <- lapply(paste0(path_to_verb_info, parsed_filenames), read_csv)
-verb_info <- do.call(rbind, verb_files) %>%
-  mutate(verbs = str_count(parsed_gloss, fixed("(VP ")) +
-           str_count(parsed_gloss, fixed("(VBP ")) -
-           str_count(parsed_gloss, fixed("(VP (VBP "))) %>%
-  distinct()
 
-write_csv(verb_info, "data/input/verbs.csv")
+for (i in c("other", "child")) {
+  verb_info <- do.call(rbind, verb_files) %>%
+    mutate(verbs = str_count(parsed_gloss, fixed("(VP ")) +
+             str_count(parsed_gloss, fixed("(VBP ")) -
+             str_count(parsed_gloss, fixed("(VP (VBP "))) %>%
+    distinct()
+  
+  filename <- ifelse(i == "child", "data/input/verbs-child.csv", 
+                     "data/input/verbs.csv")
+  
+  write_csv(as.data.frame(as.matrix(verb_info)), filename)
+}
