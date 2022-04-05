676c505a17bde7e00e3319a8fc4f603fdffe046c
Kennedy Casey <kennedybcasey@gmail.com>
2022-04-04 23:33:01 -0500

    add child speech rate

--- data-prep/prosodic-input/speech-rate.R
+++ data-prep/prosodic-input/speech-rate.R
@@ -8,8 +8,9 @@ source("data-prep/overall/get-min-dur.R")
 utterances <- read_csv("data/childes-input.csv")
 
 rate <- utterances %>%
-  filter(speaker_type == "other" & !is.na(media_start) & 
-           !is.na(media_end) & (media_end - media_start) >= min_dur/1000) %>%
+  filter(!is.na(media_start) & 
+           !is.na(media_end) & 
+           (media_end - media_start) >= min_dur/1000) %>%
   mutate(rate = num_tokens/(media_end - media_start))
 
 write_csv(rate, "data/input/rate.csv")
