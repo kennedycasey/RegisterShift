aa78d373480ac49a02b84e6243aa472e81b10c69
Kennedy Casey <kennedybcasey@gmail.com>
2022-04-04 23:40:06 -0500

    store child and adult utts separately

--- data-prep/prosodic-input/pitch.R
+++ data-prep/prosodic-input/pitch.R
@@ -8,23 +8,30 @@ source("data-prep/overall/functions.R")
 path_to_pitch_info = "data-prep/prosodic-input/processed/"
 pitch_filenames <- list.files(path = path_to_pitch_info, pattern = ".csv")
 pitch_files <- lapply(paste0(path_to_pitch_info, pitch_filenames), read_csv)
-pitch_info <- do.call(rbind, pitch_files) %>%
-  mutate(form = case_when(
-      item %in% CDS_forms ~ "CDS", 
-      item %in% ADS_forms ~ "ADS")) %>%
-  left_join(pairs %>% rename(item = word), by = "item") %>%
-  filter(across(starts_with("pitch"), ~ . != "audio file missing" &
-                  . != "relevant audio clip missing" &
-                  . != "utterance too short to analyze" &
-                  . != "--undefined--" &
-                  . != "problematic line")) %>%
-  mutate(across(starts_with("pitch"), ~ as.numeric(as.character(.)))) %>%
-  group_by(item) %>%
-  filter(media_end - media_start > 0.5) %>%
-  mutate(form = factor(form, levels = c("CDS", "ADS")), 
-         form_numeric = case_when(
-           form == "CDS" ~ 0, 
-           form == "ADS" ~ 1), 
-         pitch_range = abs(pitch_range))
 
-write_csv(as.data.frame(as.matrix(pitch_info)), "data/input/pitch.csv")
+for (i in c("other", "child")) {
+  pitch_info <- do.call(rbind, pitch_files) %>%
+    filter(speaker_type == i) %>%
+    mutate(form = case_when(
+        item %in% CDS_forms ~ "CDS", 
+        item %in% ADS_forms ~ "ADS")) %>%
+    left_join(pairs %>% rename(item = word), by = "item") %>%
+    filter(across(starts_with("pitch"), ~ . != "audio file missing" &
+                    . != "relevant audio clip missing" &
+                    . != "utterance too short to analyze" &
+                    . != "--undefined--" &
+                    . != "problematic line")) %>%
+    mutate(across(starts_with("pitch"), ~ as.numeric(as.character(.)))) %>%
+    group_by(item) %>%
+    filter(media_end - media_start > 0.5) %>%
+    mutate(form = factor(form, levels = c("CDS", "ADS")), 
+           form_numeric = case_when(
+             form == "CDS" ~ 0, 
+             form == "ADS" ~ 1), 
+           pitch_range = abs(pitch_range))
+  
+  filename <- ifelse(i == "child", "data/input/pitch-child.csv", 
+                     "data/input/pitch.csv")
+  
+  write_csv(as.data.frame(as.matrix(pitch_info)), filename)
+}
