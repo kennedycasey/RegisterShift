c4dbcd1d12110f0713642ded71b40e86eaea9905
Kennedy Casey <kennedybcasey@gmail.com>
2022-04-04 23:15:48 -0500

    rm individual scaling + other minor updates

--- data-prep/prosodic-input/pitch.R
+++ data-prep/prosodic-input/pitch.R
@@ -6,7 +6,7 @@ source("data-prep/overall/functions.R")
 # read in pitch info from data_prep folder
 # merge into single df
 path_to_pitch_info = "data-prep/prosodic-input/processed/"
-pitch_filenames <- list.files(path = path_to_pitch_info, pattern = "*.csv")
+pitch_filenames <- list.files(path = path_to_pitch_info, pattern = ".csv")
 pitch_files <- lapply(paste0(path_to_pitch_info, pitch_filenames), read_csv)
 pitch_info <- do.call(rbind, pitch_files) %>%
   mutate(form = case_when(
@@ -16,7 +16,8 @@ pitch_info <- do.call(rbind, pitch_files) %>%
   filter(across(starts_with("pitch"), ~ . != "audio file missing" &
                   . != "relevant audio clip missing" &
                   . != "utterance too short to analyze" &
-                  . != "--undefined--")) %>%
+                  . != "--undefined--" &
+                  . != "problematic line")) %>%
   mutate(across(starts_with("pitch"), ~ as.numeric(as.character(.)))) %>%
   group_by(item) %>%
   filter(media_end - media_start > 0.5) %>%
@@ -24,12 +25,6 @@ pitch_info <- do.call(rbind, pitch_files) %>%
          form_numeric = case_when(
            form == "CDS" ~ 0, 
            form == "ADS" ~ 1), 
-         pitch_range = abs(pitch_range),
-         pitch_mean_scaled = scale(pitch_mean), 
-         pitch_range_scaled = scale(pitch_range),
-         age_scaled = scale(age)) %>%
-  select(item, pair, form, form_numeric, speaker_id, target_child_id, 
-         age, age_scaled, pitch_mean, pitch_mean_scaled, pitch_range, 
-         pitch_range_scaled)
+         pitch_range = abs(pitch_range))
 
 write_csv(as.data.frame(as.matrix(pitch_info)), "data/input/pitch.csv")
