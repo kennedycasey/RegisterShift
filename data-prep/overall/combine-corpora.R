98e1b1cf883968aa8d4974a157cc8494db292f85
Kennedy Casey <kennedybcasey@gmail.com>
2022-04-12 14:21:29 -0500

    rm extra ldp cols

--- data-prep/overall/combine-corpora.R
+++ data-prep/overall/combine-corpora.R
@@ -4,14 +4,19 @@ library(tidyverse)
 childes <- read_csv("data/childes-input.csv") %>%
   mutate(id = paste0(id), 
          speaker_id = paste0(speaker_id), 
+         transcript_id = paste0(transcript_id),
          target_child_id = paste0(target_child_id))
+
+cols <- colnames(childes)
 
 # read in LDP data and update to match CHILDES cols
 ldp <- read_csv("~/Desktop/secure/ldp-input.csv") %>%
   mutate(corpus_name = "LDP", 
          target_child_id = paste0("LDP", target_child_id), 
          speaker_id = paste0(target_child_id, "_", speaker_type), 
-         id = paste0(target_child_id, "_", line))
+         transcript_id = paste0(target_child_id, "_", session),
+         id = paste0(target_child_id, "_", line)) %>%
+  select(contains(cols))
 
 # read in input predictor data
 input <- bind_rows(childes, ldp)
